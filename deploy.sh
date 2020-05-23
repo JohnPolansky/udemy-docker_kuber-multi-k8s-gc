ea28e0d94f459ba6a990f79ba62958cf86cb519a
John Polansky <John.Polansky@itential.com>
2020-05-23 02:02:13 -0500

    fixing typo in deploy.sh2

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 docker build -t johnpolansky/multi-client:latest -t johnpolansky/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t johnpolansky/multi-server:latest -t johnpolansky/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t johnpolansky/multi-worker:latest -t johnpolansky/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-dockerpush johnpolansky/multi-client:latest
-dockerpush johnpolansky/multi-server:latest
-dockerpush johnpolansky/multi-worker:latest
+docker push johnpolansky/multi-client:latest
+docker push johnpolansky/multi-server:latest
+docker push johnpolansky/multi-worker:latest
 
 docker push johnpolansky/multi-client:$SHA
 docker push johnpolansky/multi-server:$SHA
